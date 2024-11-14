934407d38339a872bc72beaf0371d98502c101a3
Fikri Alfaraby <100327546+FikriAlfaraby@users.noreply.github.com>
2024-11-15 00:10:25 +0700

    🐛 add responsive

--- src/Components/Home/Logo.jsx
+++ src/Components/Home/Logo.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 const Logo = ({ width = "60px", height = "60px" }) => {
   return (
     <Link to="/">
-      <div className="flex items-center">
+      <div className="flex items-center md:space-x-0 space-x-1">
         <img src="/logo.png" alt="logo" style={{ width, height }} />
-        <p className="font-[600] text-xl text-[#23C1C1]">
+        <p className="font-[600] md:text-xl text-base text-[#23C1C1]">
           Zero <span className="text-black font-[500]">Sugar</span>
         </p>
       </div>
