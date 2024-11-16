124d19e2f53f692007fa392a3d6a7b6615552e7e
Fikri Alfaraby <100327546+FikriAlfaraby@users.noreply.github.com>
2024-11-16 10:36:50 +0700

    🐛 fix widht icon in mobile navbar

--- src/Shared/MobileNavbar.jsx
+++ src/Shared/MobileNavbar.jsx
@@ -33,7 +33,7 @@ const MobileNavbar = ({ active }) => {
           shadow ? "shadow-lg" : ""
         }`}
       >
-        <Logo width="40px" height="40px" />
+        <Logo width="20px" height="20px" />
 
         <div className="flex items-center gap-[16px]">
           <CiSearch className="text-[1.5rem] cursor-pointer text-gray-600" />
